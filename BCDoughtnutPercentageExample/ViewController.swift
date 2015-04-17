25a365bd256b58e24952144c358c372660087744
czetsuya <czetsuya@gmail.com>
2015-04-17 11:20:52 +0800

    Initial Commit

---
+++ BCDoughtnutPercentageExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BCDoughtnutPercentageExample
+//
+//  Created by Edward P. Legaspi on 4/17/15.
+//  Copyright (c) 2015 Brood Camp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
